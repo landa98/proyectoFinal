a212d8a0affa8bbc64fc3470c2cf511bdd7d2299
AmosMVL2261 <refi2261@gmail.com>
2020-05-22 01:12:38 -0500

    Update bicicleta.cpp

--- bicicleta.cpp
+++ bicicleta.cpp
@@ -8840,12 +8840,10 @@ int main()
 
 		model = glm::mat4(1.0);
 		model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));
-		model = glm::translate(model, glm::vec3(0.0f + Bicicleta_TX, 0.0f + Bicicleta_TY, 0.0f + Bicicleta_TZ));
-
+		model = glm::translate(model, glm::vec3(-10.0f + Bicicleta_TX, 0.0f + Bicicleta_TY, 0.0f + Bicicleta_TZ));
 		//model = glm::rotate(model, (-90 + Bicicleta_RX)* toRadians, glm::vec3(1.0f, 0.0f, 0.0f));
 		//model = glm::rotate(model, ( 90 + Bicicleta_RY)* toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
 		model = glm::rotate(model, (Bicicleta_RZ)* toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
-
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		Material_brillante.UseMaterial(uniformSpecularIntensity, uniformShininess);
 		Bicicleta.RenderModel();
